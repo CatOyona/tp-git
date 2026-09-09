e483b64243db47fee65035f7e363299cc44ba269
Ismaël <ismael.avon@etu.univ-lyon1.fr>
2026-09-09 10:55:07 +0200

    Revert "Readme"
    This reverts commit f12b4cd6a822f8549445ec280666789901d26ff1.

--- calcul.py
+++
@@ -1,9 +0,0 @@
-def moyenne (valeurs):
-    n = 0
-    sum = 0
-    for i in range(len(valeurs)):
-        sum += valeurs[i]
-        n += 1
-    return sum/n
-
-print("Tu as comme moyenne: ", moyenne([8, 15, 20, 5]))
